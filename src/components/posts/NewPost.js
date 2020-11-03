1c1c9d244b11297ea55223fae69470fc2a69a108
Matt Logan <mlogan5212@gmail.com>
2020-11-02 18:43:04 -0600

    wip

--- src/components/posts/NewPost.js
+++ src/components/posts/NewPost.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class NewPost extends React.Component {
+  state = {
+    user_id: '',
+    category_id: '',
+    title: '',
+    content: '',
+    header_img: '',
+  }
+  
   render() {
     return (
       <h1 className="text-center mt-3">Create New Post</h1>
