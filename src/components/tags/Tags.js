31696f885f74b07cd04b87ffc92145f03e7597b2
Nick Walters <nswalters.s@gmail.com>
2020-11-02 20:21:50 -0600

    Added routes and components for tags

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react"
 
-class Tags extends React.Component {
-    render() {
-        return (
-            <h1 className="text-center mt-3">View All Tags</h1>
-        )
-    }
+export const Tags = () => {
+    return (
+        <h1 className="text-center mt-3">View All Tags</h1>
+    )
 }
-
-export default Tags;
