c5bab207c471e4dbcafadcc7915720ba7fc03342
Nick Walters <nswalters.s@gmail.com>
2020-11-07 12:48:06 -0600

    Get tags for a post and display on single post view

--- src/components/posts/SinglePost.js
+++ src/components/posts/SinglePost.js
@@ -3,25 +3,38 @@ import { withRouter } from 'react-router-dom'
 import moment from 'moment';
 import { confirmAlert } from 'react-confirm-alert';
 
+import Tag from '../tags/Tag';
+
 import "./SinglePost.css";
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
 class SinglePost extends React.Component {
   state = {
     post: {},
+    post_tags: {},
   }
 
   componentDidMount() {
     this.getPostById()
+    this.getPostTags()
+  }
+
+  getPostTags = () => {
+    const { postId } = this.props.match.params;
+    fetch(`http://localhost:8088/post_tags/${postId}`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ post_tags: res })
+      })
   }
 
   getPostById = () => {
     const { postId } = this.props.match.params;
     return fetch(`http://localhost:8088/posts/${postId}`)
-    .then(res => res.json())
-    .then(res => {
-      this.setState({ post: res })
-    })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ post: res })
+      })
   }
 
   handleClickDelete = () => {
@@ -40,8 +53,8 @@ class SinglePost extends React.Component {
           <div className='custom-ui'>
             <h1>Are you sure?</h1>
             <p>You want to delete this post?</p>
-            <button className ="mr-3 dialog-btn" onClick={onClose}><h5 className="dialog-txt">No</h5></button>
-            <button className ="dialog-btn"
+            <button className="mr-3 dialog-btn" onClick={onClose}><h5 className="dialog-txt">No</h5></button>
+            <button className="dialog-btn"
               onClick={() => {
                 this.handleClickDelete();
                 onClose();
@@ -57,7 +70,7 @@ class SinglePost extends React.Component {
 
   render() {
     const { postId } = this.props.match.params;
-    const { post } = this.state;
+    const { post, post_tags } = this.state;
     const strPost = JSON.stringify(post)
     const pub_date = moment(post.publication_date).format('MMM Do, YYYY');
     return (
@@ -65,21 +78,21 @@ class SinglePost extends React.Component {
         <div className="post-heading">
           <h1>{post.title}</h1>
           <div className="post-controls">
-            <i class="fas fa-trash-alt mr-3"  onClick={this.submit}></i>
-            <i class="fas fa-edit"></i>
+            <i className="fas fa-trash-alt mr-3" onClick={this.submit}></i>
+            <i className="fas fa-edit"></i>
           </div>
         </div>
         {
           post.header_img ? (
             <img src={post.header_img} />
           ) : (
-            <p><i>No Header Image</i></p>
-          )
+              <p><i>No Header Image</i></p>
+            )
         }
         {
           post.user ? (
             <div className="post-author">
-              <i class="fas fa-user mr-2"></i>
+              <i className="fas fa-user mr-2"></i>
               <h4 className="author-name">{post.user.display_name}</h4>
               <h6 className="author-name lg ml-5">{pub_date}</h6>
             </div>
@@ -87,6 +100,17 @@ class SinglePost extends React.Component {
               ''
             )
         }
+        <div className="d-flex flex-row">
+          <small className="mr-3"><strong><em>Tags: </em></strong></small>
+          {
+            post_tags.length > -1 ? (
+              post_tags.map((pt) => (<Tag key={pt.id} pt={pt} />))
+            ) : (
+                ''
+              )
+          }
+        </div>
+        <i class="fas fa-tags p-2 mb-3 mt-2 rounded-lg manage-tags-button">Manage Tags</i>
         <h6 className="mt-4">{post.content}</h6>
       </div>
     )
