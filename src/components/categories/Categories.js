53554cfd7e4cfc51dd8593b9d9202c249e14a813
olamide oladinni <olajide1987@gmail.com>
2020-11-10 16:46:37 -0600

    wip

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {  withRouter } from 'react-router-dom';
+import './Categories.css'
 
 class Categories extends React.Component {
   render() {
+    const { categories } = this.props;
     return (
-      <h1 className="text-center mt-3">Category Managment</h1>
+      <div className="categories-list">
+        <h5>{categories.name}</h5>
+      </div>
     )
   }
 }
 
-export default Categories;
+export default withRouter(Categories);
--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -14,7 +14,7 @@ componentDidMount() {
 }
 
 getAllCategories = () => {
-  return fetch("http:/localhost.8088/categories")
+  return fetch("http://localhost:8088/categories")
   .then(res => res.json())
   .then(res => {
     this.setState({ categories: res })
