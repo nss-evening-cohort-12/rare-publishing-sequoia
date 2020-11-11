3ebab7de29a00c050c6ad184ad0ed466709c25d2
Nick Walters <nswalters.s@gmail.com>
2020-11-10 18:41:59 -0600

    FIXES FOR OLA TO COMPARE AGAINST

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import {  withRouter } from 'react-router-dom';
+
+import Category from './Category'
+
 import './Categories.css'
 
 class Categories extends React.Component {
-  render() {
-    const { categories } = this.props;
-    return (
-      <div className="categories-list">
-        <h5>{categories.name}</h5>
-      </div>
-    )
+  state = {
+    categories: [],
   }
+
+componentDidMount() {
+  this.getAllCategories();
+}
+
+getAllCategories = () => {
+  return fetch("http://localhost:8088/categories")
+  .then(res => res.json())
+  .then(res => {
+    this.setState({ categories: res })
+  })
+}
+
+  render() {
+    const { categories } = this.state;
+    const myCategory = categories.map((category) => <Category key={category.id} categories={category} />)
+    return (
+      <div>
+      <h1 className="text-center mt-3">View All Categories</h1>
+      <div className="categories-container">
+        {myCategory}
+      </div>
+    </div>
+
+
+  )
+ }
 }
 
 export default withRouter(Categories);
--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,40 +1,15 @@
 import React from 'react';
-
-import Categories from './Categories'
-
 import './Categories.css'
 
 class Category extends React.Component {
-  state = {
-    Categories: [],
-  }
-
-componentDidMount() {
-  this.getAllCategories();
-}
-
-getAllCategories = () => {
-  return fetch("http://localhost:8088/categories")
-  .then(res => res.json())
-  .then(res => {
-    this.setState({ categories: res })
-  })
-}
-
   render() {
-    const { category } = this.state;
-    const myCategory = category.map((categories) => <Categories key={categories.id} categories={categories} />)
+    const { categories } = this.props;
     return (
-      <div>
-      <h1 className="text-center mt-3">View All Categories</h1>
-      <div className="categories-container">
-        {myCategory}
+      <div className="categories-list">
+        <h5>{categories.name}</h5>
       </div>
-    </div>
-
-
-  )
- }
+    )
+  }
 }
 
 export default Category;
